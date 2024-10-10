faac8a54e16f93cda45da7b831bdfd56cfc50426
Kacper Grabczak <kacpergrabczak@gmail.com>
2024-10-10 21:16:55 +0200

    Disable CORS

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.after_request
+def request_callback(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
