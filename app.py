5ef8a6f6820e2114e00ab8d1b605deb6664e419f
Kacper Grabczak <kacpergrabczak@gmail.com>
2024-10-26 23:46:56 +0200

    Add request json printing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 
+import json
 import random
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ def play():
 
     data = request.json.get('data')
 
+    print(json.dumps(data, indent = 4))
+
     graph = data.get('graph')
 
     edges = graph.get('edges')
@@ -26,5 +29,7 @@ def play():
     available_edges = list(filter(lambda x: x.get('team') == 'none', edges))
 
     random_edge = random.choice(available_edges)
+
+    target_clique_size = data.get('target_clique_size')
 
     return random_edge
